816edd1d74f5899a2672ac4d786c05bccdd2dbae
Lyle Keeton <lyle@effectivesln.com>
2023-02-20 17:18:39 -0600

    init commit

---
+++ ConsoleApp1/NeuralNet.cs
@@ -0,0 +1,147 @@
+﻿using System;
+namespace ConsoleApp1;
+
+class NeuralNet
+{
+    private int inputSize;
+    private int hiddenSize;
+    private int outputSize;
+    private double[,] weights1;
+    private double[,] weights2;
+    private double[] bias1;
+    private double[] bias2;
+
+    public NeuralNet(int inputSize, int hiddenSize, int outputSize)
+    {
+        this.inputSize = inputSize;
+        this.hiddenSize = hiddenSize;
+        this.outputSize = outputSize;
+
+        Random rand = new Random();
+
+        // Initialize weights and biases for the hidden layer
+        this.weights1 = new double[inputSize, hiddenSize];
+        this.bias1 = new double[hiddenSize];
+        for (int i = 0; i < inputSize; i++)
+        {
+            for (int j = 0; j < hiddenSize; j++)
+            {
+                this.weights1[i, j] = rand.NextDouble() * 2 - 1;
+            }
+        }
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            this.bias1[i] = rand.NextDouble() * 2 - 1;
+        }
+
+        // Initialize weights and biases for the output layer
+        this.weights2 = new double[hiddenSize, outputSize];
+        this.bias2 = new double[outputSize];
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            for (int j = 0; j < outputSize; j++)
+            {
+                this.weights2[i, j] = rand.NextDouble() * 2 - 1;
+            }
+        }
+        for (int i = 0; i < outputSize; i++)
+        {
+            this.bias2[i] = rand.NextDouble() * 2 - 1;
+        }
+    }
+
+    public double[] Forward(double[] inputs)
+    {
+        // Calculate output of hidden layer
+        double[] hidden = new double[hiddenSize];
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < inputSize; j++)
+            {
+                sum += inputs[j] * weights1[j, i];
+            }
+            hidden[i] = Sigmoid(sum + bias1[i]);
+        }
+
+        // Calculate output of output layer
+        double[] outputs = new double[outputSize];
+        for (int i = 0; i < outputSize; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < hiddenSize; j++)
+            {
+                sum += hidden[j] * weights2[j, i];
+            }
+            outputs[i] = Sigmoid(sum + bias2[i]);
+        }
+        return outputs;
+    }
+
+    public void Train(double[] inputs, double[] targets, double learningRate)
+    {
+        double[] hidden = new double[hiddenSize];
+        double[] outputs = new double[outputSize];
+
+        // Forward pass
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < inputSize; j++)
+            {
+                sum += inputs[j] * weights1[j, i];
+            }
+            hidden[i] = Sigmoid(sum + bias1[i]);
+        }
+
+        for (int i = 0; i < outputSize; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < hiddenSize; j++)
+            {
+                sum += hidden[j] * weights2[j, i];
+            }
+            outputs[i] = Sigmoid(sum + bias2[i]);
+        }
+
+        // Backward pass
+        double[] outputError = new double[outputSize];
+        double[] hiddenError = new double[hiddenSize];
+
+        for (int i = 0; i < outputSize; i++)
+        {
+            outputError[i] = (targets[i] - outputs[i]) * SigmoidDerivative(outputs[i]);
+            for (int j = 0; j < hiddenSize; j++)
+            {
+                weights2[j, i] += learningRate * hidden[j] * outputError[i];
+            }
+            bias2[i] += learningRate * outputError[i];
+        }
+
+        for (int i = 0; i < hiddenSize; i++)
+        {
+            double sum = 0;
+            for (int j = 0; j < outputSize; j++)
+            {
+                sum += outputError[j] * weights2[i, j];
+            }
+            hiddenError[i] = sum * SigmoidDerivative(hidden[i]);
+
+            for (int j = 0; j < inputSize; j++)
+            {
+                weights1[j, i] += learningRate * inputs[j] * hiddenError[i];
+            }
+            bias1[i] += learningRate * hiddenError[i];
+        }
+    }
+
+    private static double Sigmoid(double x)
+    {
+        return 1 / (1 + Math.Exp(-x));
+    }
+
+    private static double SigmoidDerivative(double x)
+    {
+        return x * (1 - x);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using ConsoleApp1;
+ 
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Define neural network parameters
+        int inputSize = 2;
+        int hiddenSize = 4;
+        int outputSize = 1;
+        double learningRate = 0.1;
+
+        // Create neural network
+        NeuralNet net = new NeuralNet(inputSize, hiddenSize, outputSize);
+
+        // Train neural network on dataset
+        double[,] inputs = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 }
+        };
+        double[] targets = { 0, 1, 1, 0 };
+        for (int epoch = 0; epoch < 10000; epoch++)
+        {
+            for (int i = 0; i < inputs.GetLength(0); i++)
+            {
+                double[] input = new double[inputSize];
+                for (int j = 0; j < inputSize; j++)
+                {
+                    input[j] = inputs[i, j];
+                }
+                double[] target = { targets[i] };
+                net.Train(input, target, learningRate);
+            }
+        }
+
+        // Test neural network on new inputs
+        Console.WriteLine("0 XOR 0 = " + net.Forward(new double[] { 0, 0 })[0]);
+        Console.WriteLine("0 XOR 1 = " + net.Forward(new double[] { 0, 1 })[0]);
+        Console.WriteLine("1 XOR 0 = " + net.Forward(new double[] { 1, 0 })[0]);
+        Console.WriteLine("1 XOR 1 = " + net.Forward(new double[] { 1, 1 })[0]);
+
+
+
+    }
+
+}
